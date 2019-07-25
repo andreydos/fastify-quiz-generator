5047d1130a2ecae823c6f25ee755e1c4b00651cf
andreydos <andrii3110@gmail.com>
2019-07-25 15:36:51 +0300

    data fields update

--- data.js
+++ data.js
@@ -4,39 +4,39 @@ module.exports = {
     1: [
       {
         question: 'Smallest bird?',
-        answer: { title: 'Hummingbird Bee', id: 2 },
-        options: [{ title: 'Pardalote', id: 1 }, { title: 'Hummingbird Bee', id: 2 }, {
-          title: 'Gerigón Piquicorto',
+        answer: { text: 'Hummingbird Bee', id: 2 },
+        options: [{ text: 'Pardalote', id: 1 }, { text: 'Hummingbird Bee', id: 2 }, {
+          text: 'Gerigón Piquicorto',
           id: 3,
-        }, { title: 'Golden-headed Cisticola', id: 4 }],
+        }, { text: 'Golden-headed Cisticola', id: 4 }],
       },
       {
         question: 'Biggest bird?',
-        answer: { title: 'Ostrich', id: 3 },
-        options: [{ title: 'Emu', id: 1 }, { title: 'Southern cassowary', id: 2 }, {
-          title: 'Ostrich',
+        answer: { text: 'Ostrich', id: 3 },
+        options: [{ text: 'Emu', id: 1 }, { text: 'Southern cassowary', id: 2 }, {
+          text: 'Ostrich',
           id: 3,
-        }, { title: 'Emperor penguin', id: 4 }],
+        }, { text: 'Emperor penguin', id: 4 }],
       },
     ],
     2: [
       {
         question: 'Who invented the first car in the world?',
-        answer: { title: 'Karl Benz', id: 1 },
-        options: [{ title: 'Karl Benz', id: 1 }, { title: 'George Selden', id: 2 }, {
-          title: 'Gottlieb Daimler',
+        answer: { text: 'Karl Benz', id: 1 },
+        options: [{ text: 'Karl Benz', id: 1 }, { text: 'George Selden', id: 2 }, {
+          text: 'Gottlieb Daimler',
           id: 3,
-        }, { title: 'Charles Duryea', id: 4 }],
+        }, { text: 'Charles Duryea', id: 4 }],
       },
     ],
     3: [
       {
         question: 'Biggest airplane?',
-        answer: { title: 'Antonov AN-225 Mriya', id: 3 },
-        options: [{ title: 'Airbus A380-800', id: 1 }, { title: 'Boeing 747-8i', id: 2 }, {
-          title: 'Antonov AN-225 Mriya',
+        answer: { text: 'Antonov AN-225 Mriya', id: 3 },
+        options: [{ text: 'Airbus A380-800', id: 1 }, { text: 'Boeing 747-8i', id: 2 }, {
+          text: 'Antonov AN-225 Mriya',
           id: 3,
-        }, { title: 'Antonov An-124', id: 4 }],
+        }, { text: 'Antonov An-124', id: 4 }],
       },
     ],
   },
